899a89ab2a890e34dcade48fe55da2810cf58424
Marcel Greter <marcel.greter@ocbnet.ch>
2024-08-26 16:49:25 +0200

    Add (commented out) test block

---
+++ Harmony/BlockOcbLcbRepairTest.cs
@@ -0,0 +1,10 @@
+﻿// Simple class to test repair speed in game
+// When placing this block it well be damaged
+public class BlockOcbLcbRepairTest : Block
+{
+    public override void PlaceBlock(WorldBase _world, BlockPlacement.Result _result, EntityAlive _ea)
+    {
+        _result.blockValue.damage = 5000;
+        base.PlaceBlock(_world, _result, _ea);
+    }
+}
